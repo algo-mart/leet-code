9dd571d2b52dd033767a4ca89040704a355e7d9f
Olusola Alao <alaoolusolae@gmail.com>
2022-01-23 04:55:05 +0100

    add microsoft assessment

--- microsoft/microsoft.go
+++ microsoft/microsoft.go
@@ -1,9 +1,4 @@
-package main
-
-import (
-	"fmt"
-	"math"
-)
+package microsoft
 
 // you can also use imports, for example:
 // import "fmt"
@@ -12,26 +7,24 @@ import (
 // you can write to stdout for debugging purposes, e.g.
 // fmt.Println("this is a debug message")
 
-
-
 func Solution6(blocks []int) int {
 	// write your code in Go 1.4
-	a:= 0
+	a := 0
 	max := 0
 
-	for a < len(blocks){
-		right:= a
-		left:= a
-		for right < len(blocks)-1 && blocks[right] <= blocks[right+1]{
+	for a < len(blocks) {
+		right := a
+		left := a
+		for right < len(blocks)-1 && blocks[right] <= blocks[right+1] {
 			right++
 		}
-		for left > 0 && blocks[left] <= blocks[left-1]{
+		for left > 0 && blocks[left] <= blocks[left-1] {
 			left--
 		}
-		if right-left > max{
-			max = right-left
+		if right-left > max {
+			max = right - left
 		}
-		a = right+1
+		a = right + 1
 	}
 	return max + 1
 }
@@ -40,7 +33,7 @@ func Solution7(text string) int {
 	// write your code in Go 1.4
 	store := map[rune]int{'b': 0, 'a': 0, 'l': 0, 'o': 0, 'n': 0}
 	a := 0
-	min := math.MaxInt64
+	min := 0
 	for a < len(text) {
 		if _, ok := store[rune(text[a])]; ok {
 			store[rune(text[a])]++
@@ -55,11 +48,4 @@ func Solution7(text string) int {
 		}
 	}
 	return min
-}
-
-func main() {
-	fmt.Println(Solution7("nlaebolko"))
-	//fmt.Println(Solution([]int {2, 5, 5, 2, 6}))
-	//fmt.Println(Solution([]int {1,1}))
-	//fmt.Println(Solution([]int {1,1}))
 }
